9744418ff00e5b00ba4b5040c74d83921e99f4ad
Avvyxx <luizgurrola@avyx.dev>
2023-02-19 22:24:59 -0800

    condensed statement and implemented message interpreter

--- server/listeners/socket-readable/index.js
+++ server/listeners/socket-readable/index.js
@@ -2,29 +2,22 @@ const fs = require('node:fs');
 
 const fromDataFrame = require('./from-data-frame');
 const toDataFrame = require('./to-data-frame');
+const interpretMessage = require('./interpret-message');
 const { logMessage, mainDir, clearLog } = require('../../util');
 
 module.exports = (socket, sockets) => {
 	const { opcode, MASK, maskingKey, encodedPayload } = fromDataFrame(socket);
 
 	if (opcode === 1) {
-		// TODO: is there a way to condense this V
+		let message;
 		if (Boolean(MASK)) {
 			const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
-			const readableMsg = String.fromCharCode(...decodedMsg);
-
-			if (readableMsg === 'clear message log') {
-				clearLog();
-			} else {
-				logMessage(readableMsg);
-			}
+			message = String.fromCharCode(...decodedMsg);
 		} else {
-			if (encodedPayload === 'clear message log') {
-				clearLog();
-			} else {
-				logMessage(encodedPayload);
-			}
+			message = encodedPayload;
 		}
+
+		interpretMessage(message);
 
 		const updatedLog = fs.readFileSync(`${mainDir}\\server\\message-log.json`, { encoding: 'utf-8' });
 		const updatedLogFrame = toDataFrame('1', '0', '0', '0', '1', '0', updatedLog);
