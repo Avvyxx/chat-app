69a37c82eafaa3149f527ae320d71f5f27edd2ee
Avvyxx <luizgurrola@avyx.dev>
2023-02-19 22:25:25 -0800

    updated exports for util

--- server/util/index.js
+++ server/util/index.js
@@ -1,6 +1,4 @@
 module.exports = {
-	clearLog: require('./clear-log'),
-	logMessage: require('./log-message'),
 	magicWebSocketKey: require('./magic-websocket-key'),
 	mainDir: require('./main-dir'),
 	mimeDict: require('./mime-dict'),
