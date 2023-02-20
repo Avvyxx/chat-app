7c911b17af72bb24e524ff60bfba795e2cfbc9f8
Avvyxx <luizgurrola@avyx.dev>
2023-02-19 22:21:04 -0800

    clear log moved to command sub-util dir

--- server/util/commands/clear-log.js
+++ server/util/commands/clear-log.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 
-const mainDir = require('./main-dir');
+const mainDir = require('../main-dir');
 
 module.exports = () => {
 	fs.writeFileSync(`${mainDir}\\server\\message-log.json`, '[]', { encoding: 'utf-8' });
