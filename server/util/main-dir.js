38001eb9078ad685da5cb33f8e74dd8394eb2e6b
Avvyxx <luizgurrola@avyx.dev>
2023-02-22 21:56:10 -0800

    linux compatibility fix

--- server/util/main-dir.js
+++ server/util/main-dir.js
@@ -2,4 +2,4 @@ const os = require('node:os');
 
 const system = os.platform();
 
-module.exports = system === 'win32' ? __dirname.split('\\').slice(0, -2).join('\\') : system === 'linux' ? __dirname.split('\\').slice(0, -2).join('\\') : null;
+module.exports = system === 'win32' ? __dirname.split('\\').slice(0, -2).join('\\') : system === 'linux' ? __dirname.split('/').slice(0, -2).join('/') : null;
