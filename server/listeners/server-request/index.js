5b96cf2ed4ff6ce78f3169d861bff0079f3c4eae
Avvyxx <luizgurrola@avyx.dev>
2023-02-24 01:51:12 -0800

    converted server request listener function to a single function

--- server/listeners/server-request/index.js
+++ server/listeners/server-request/index.js
@@ -1,10 +1,27 @@
-const get = require('./get');
-const post = require('./post');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const { mainDir, mimeDict } = require('../../util');
 
 module.exports = (req, res) => {
 	if (req.method === 'GET') {
-		get(req, res);
-	} else if (req.method === 'POST') {
-		post(req, res);
+		const requestedFile = `${mainDir}/site${req.url === '/' ? '/index.html' : req.url}`;
+
+		if (fs.existsSync(requestedFile)) {
+			const fileContent = fs.readFileSync(requestedFile, { encoding: 'utf-8' });
+			const headers = {
+				'Content-Type': mimeDict[path.extname(requestedFile)],
+			};
+
+			res.writeHead(200, headers);
+			res.end(fileContent);
+		} else {
+			const headers = {
+				'Content-Type': 'text/plain',
+			};
+
+			res.writeHead(404, headers);
+			res.end('404');
+		}
 	}
 };
