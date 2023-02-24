17af87b6e68040b9d1c141e55ce4264e4df84d88
Avvyxx <luizgurrola@avyx.dev>
2023-02-24 01:45:34 -0800

    conditional logging based on opcode, updated system to update client logs

--- server/listeners/socket-readable/index.js
+++ server/listeners/socket-readable/index.js
@@ -1,29 +1,29 @@
-const fs = require('node:fs');
-
-const fromDataFrame = require('./from-data-frame');
-const toDataFrame = require('./to-data-frame');
 const interpretMessage = require('./interpret-message');
-const { mainDir } = require('../../util');
+const { fromDataFrame, updateClientLogs } = require('../../util');
 
 module.exports = (socket, sockets) => {
 	const data = JSON.parse(JSON.stringify(socket.read()) || {}).data;
+
 	if (data !== null) {
 		const { opcode, MASK, maskingKey, encodedPayload } = fromDataFrame(data);
 
-		if (opcode === 1) {
-			let message;
-			if (Boolean(MASK)) {
-				const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
-				message = String.fromCharCode(...decodedMsg);
-			} else {
-				message = encodedPayload;
-			}
+		let message;
+		if (Boolean(MASK)) {
+			const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
+			message = String.fromCharCode(...decodedMsg);
+		} else {
+			message = encodedPayload;
+		}
 
+		if (opcode === 1) {
 			interpretMessage(message);
 
-			const updatedLog = fs.readFileSync(`${mainDir}/server/message-log.json`, { encoding: 'utf-8' });
-			const updatedLogFrame = toDataFrame('1', '0', '0', '0', '1', '0', updatedLog);
-			sockets.forEach((a) => a.write(updatedLogFrame));
+			sockets.forEach(updateClientLogs);
+		} else if (opcode === 2) {
+			const base64Encoding = Buffer.from(message).toString('base64');
+			interpretMessage(base64Encoding);
+
+			sockets.forEach(updateClientLogs);
 		} else if (opcode === 8) {
 			socket.destroy();
 		}
