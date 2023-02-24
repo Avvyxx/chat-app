866787f2bfaabe68fc3433b78958b4e1085ccec7
Avvyxx <luizgurrola@avyx.dev>
2023-02-23 21:28:31 -0800

    readable will only process message if message is not null

--- server/listeners/socket-readable/index.js
+++ server/listeners/socket-readable/index.js
@@ -7,23 +7,25 @@ const { mainDir } = require('../../util');
 
 module.exports = (socket, sockets) => {
 	const data = JSON.parse(JSON.stringify(socket.read()) || {}).data
-	const { opcode, MASK, maskingKey, encodedPayload } = fromDataFrame(data);
+	if (data !== null) {
+		const { opcode, MASK, maskingKey, encodedPayload } = fromDataFrame(data);
 
-	if (opcode === 1) {
-		let message;
-		if (Boolean(MASK)) {
-			const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
-			message = String.fromCharCode(...decodedMsg);
-		} else {
-			message = encodedPayload;
+		if (opcode === 1) {
+			let message;
+			if (Boolean(MASK)) {
+				const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
+				message = String.fromCharCode(...decodedMsg);
+			} else {
+				message = encodedPayload;
+			}
+
+			interpretMessage(message);
+
+			const updatedLog = fs.readFileSync(`${mainDir}/server/message-log.json`, { encoding: 'utf-8' });
+			const updatedLogFrame = toDataFrame('1', '0', '0', '0', '1', '0', updatedLog);
+			sockets.forEach((a) => a.write(updatedLogFrame));
+		} else if (opcode === 8) {
+			socket.destroy();
 		}
-
-		interpretMessage(message);
-
-		const updatedLog = fs.readFileSync(`${mainDir}/server/message-log.json`, { encoding: 'utf-8' });
-		const updatedLogFrame = toDataFrame('1', '0', '0', '0', '1', '0', updatedLog);
-		sockets.forEach((a) => a.write(updatedLogFrame));
-	} else if (opcode === 8) {
-		socket.destroy();
 	}
 };
