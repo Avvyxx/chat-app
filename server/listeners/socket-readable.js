d58cfe2c9ce71265ad355b124ac739523aed29a7
Avvyxx <luizgurrola@avyx.dev>
2023-02-24 10:50:29 -0800

    refactored socket readable listener

--- server/listeners/socket-readable.js
+++ server/listeners/socket-readable.js
@@ -1,43 +1,27 @@
-const logMessage = require('../commands/log-message');
-const { fromDataFrame, updateClientLogs } = require('../util');
-
-let messageQueue = [];
+const { logMessage } = require('../commands');
+const { fromDataFrame, decodePayload, updateClientLogs } = require('../util');
 
 module.exports = (socket, sockets) => {
 	return () => {
-		const data = JSON.parse(JSON.stringify(socket.read()) || {}).data;
+		const checkIfBufferNull = socket.read();
 
-		if (data !== null) {
+		if (checkIfBufferNull) {
+			const { data } = checkIfBufferNull.toJSON();
+
 			// TODO: this function directly edits data variable
 			const { opcode, MASK, maskingKey, encodedPayload } = fromDataFrame(data);
 
-			let message;
-			if (Boolean(MASK)) {
-				const decodedMsg = Uint8Array.from(encodedPayload, (elt, i) => elt ^ maskingKey[i % 4]);
-				message = String.fromCharCode(...decodedMsg);
-			} else {
-				message = encodedPayload;
-			}
-
-			messageQueue.push(message);
+			const message = decodePayload(Boolean(MASK), maskingKey, encodedPayload);
 
 			if (opcode === 1) {
-				if (messageQueue.length === 2) {
-					logMessage(messageQueue);
+				logMessage(opcode, message);
 
-					sockets.forEach(updateClientLogs);
-
-					messageQueue = [];
-				}
+				sockets.forEach(updateClientLogs);
 			} else if (opcode === 2) {
-				if (messageQueue.length === 2) {
-					messageQueue[1] = Buffer.from(messageQueue[1]).toString('base64');
-					logMessage(messageQueue);
+				const base64Encoding = Buffer.from(message).toString('base64');
+				logMessage(opcode, base64Encoding);
 
-					sockets.forEach(updateClientLogs);
-
-					messageQueue = [];
-				}
+				sockets.forEach(updateClientLogs);
 			} else if (opcode === 8) {
 				socket.destroy();
 			}
