e571c85384669adfb369957bfd445f63ec96f10a
Avvyxx <luizgurrola@avyx.dev>
2023-02-27 18:03:33 -0800

    fixed maximum call stack error

--- server/util/decode-payload.js
+++ server/util/decode-payload.js
@@ -1,7 +1,13 @@
 module.exports = (isMasked, maskingKey, data) => {
 	if (isMasked) {
+		let str = '';
 		const decodedMsg = Uint8Array.from(data, (elt, i) => elt ^ maskingKey[i % 4]);
-		return String.fromCharCode(...decodedMsg);
+
+		decodedMsg.forEach((charCode) => {
+			str += String.fromCharCode(charCode);
+		});
+
+		return str;
 	} else {
 		return data;
 	}
