a1e56ee145baa8900ee40871c4a3d95ca47840cc
Avvyxx <luizgurrola@avyx.dev>
2023-02-27 22:36:51 -0800

    fixed main dir logic

--- server/util/constants/main-dir.js
+++ server/util/constants/main-dir.js
@@ -4,4 +4,4 @@ const system = os.platform();
 
 const whichSlash = system === 'win32' ? '\\' : system === 'linux' ? '/' : null;
 
-module.exports = __dirname.split(whichSlash).slice(0, -2).join(whichSlash);
+module.exports = __dirname.split(whichSlash).slice(0, -3).join(whichSlash);
