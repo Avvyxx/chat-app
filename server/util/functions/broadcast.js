224ddf38e5978dd714abefbf76ff3267bc1164a1
Avvyxx <luizgurrola@avyx.dev>
2023-03-01 15:04:53 -0800

    created function to broadcast message to all connected sockets

---
+++ server/util/functions/broadcast.js
@@ -0,0 +1,10 @@
+const { toDataFrame } = require('../index');
+
+// TODO: implement this broadcast function where necessary
+module.exports = (socketArr, message) => {
+	const frame = toDataFrame('1', '0', '0', '0', '1', '0', message);
+
+	socketArr.forEach((socket) => {
+		socket.write(frame);
+	});
+};
