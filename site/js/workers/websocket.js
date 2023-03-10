b3d8b60952ace4b6de78dd9f04844153b606661c
Avvyxx <luizgurrola@avyx.dev>
2023-03-10 14:31:54 -0800

    server functionality

--- site/js/workers/websocket.js
+++ site/js/workers/websocket.js
@@ -3,7 +3,7 @@ let webSocket;
 onmessage = (e) => {
 	switch (e.data.webSocketWorkerObjective) {
 		case 'initiate websocket':
-			webSocket = new WebSocket('ws://localhost:3000');
+			webSocket = new WebSocket('wss://avyx.dev:3000');
 
 			webSocket.onopen = () => {
 				console.log('WebSocket connection to chat server established.');
