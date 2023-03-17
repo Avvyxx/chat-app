06b52223ba51bb184df75e364cbade6d59bce55c
Avvyxx <luizgurrola@avyx.dev>
2023-03-17 14:24:46 -0700

    expanded mime dict

--- server/util/constants/mime-dict.js
+++ server/util/constants/mime-dict.js
@@ -1,7 +1,8 @@
 module.exports = {
-	'.html': 'text/html',
-	'.css': 'text/css',
-	'.js': 'text/javascript',
-	'.png': 'image/png',
-	'.jpg': 'image/jpg',
+    '.html': 'text/html',
+    '.css': 'text/css',
+    '.js': 'text/javascript',
+    '.png': 'image/png',
+    '.jpg': 'image/jpg',
+    '.txt': 'text/plain'
 };
